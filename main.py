931f47b39d792f55159c08b291d7105877b3bf7d
NotandorSS <jkmorris@udel.edu>
2020-07-15 17:26:04 -0400

    updating hardCodedInfo

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import requests
 from hardCodedInfo import threads, users
 from ScraperService import scrapeThread
 
-testRequest = {
-	'secretrealm': ['secret'],
-	'MarbleSplotch': ['Hay'],
-}
-
-testLinks = ['https://www.deviantart.com/comments/1/840703897/4840503638']
-
-scrapeThread(testLinks, testRequest)
+scrapeThread(threads, users)
