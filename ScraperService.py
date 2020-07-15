5b5932772a1c33514b307aa5e42ae2f39bf173c9
NotandorSS <jkmorris@udel.edu>
2020-07-15 17:03:08 -0400

    refactored

---
+++ ScraperService.py
@@ -0,0 +1,36 @@
+import requests
+
+#startingLinks = list of links
+#searchRequests = {'searchTerm': ['requestedBy/tab']}
+def scrapeThread(startingLinks, searchRequests):
+
+    s = requests.Session()
+    s.params.update({
+        'typeid': '1',
+        'maxdepth': '6',
+        'order': 'newest',
+        'limit': '50'
+    })
+    for link in startingLinks:
+        thread = link[38:].split('/')
+        threadId = thread[0]
+        cursor = thread[1]+"+"
+        
+        while True:
+            threadSegment = s.get("https://www.deviantart.com/_napi/shared_api/comments/thread", params={'itemid': threadId, 'cursor': cursor}).json()
+            for comment in threadSegment['thread']:
+                username = comment['user']['username']
+                if username in searchRequests:
+                    #write to file we got a link
+                    print(username+" posted: ")
+                    print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+                else:
+                    for searchTerm in searchRequests:
+                        if searchTerm in comment['textContent']['html']['markup']:
+                            #write to file we got a link
+                            print(searchTerm+" mentioned in: ")
+                            print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+            cursor = threadSegment['cursor']
+            if not threadSegment['hasMore']:
+                break
+    
--- fileReader.py
+++ fileReader.py
@@ -1,7 +1,17 @@
-f = open('results.txt', 'r')
-line = f.readline()
+# f = open('results.txt', 'r')
+# line = f.readline()
 
-array = line.split('(')
+# array = line.split('(')
 
-for arr in array:
-    print(arr.replace("', '", "\n"))
+# for arr in array:
+#     print(arr.replace("', '", "\n"))
+test = ['linkA', 'linkB']
+f = open("demofile2.txt", "w")
+f.write("Now the file has more content!")
+f.write("And even more content")
+f.writelines(test)
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
