e9fa460eaf14dab6639957eb51eb2815d9548a71
NotandorSS <jkmorris@udel.edu>
2020-07-15 18:50:15 -0400

    dummy commit

--- ScraperService.py
+++ ScraperService.py
@@ -16,20 +16,26 @@ def scrapeThread(startingLinks, searchRequests):
         threadId = thread[0]
         cursor = thread[1]+"+"
         
+        count = 0
+
         while True:
             threadSegment = s.get("https://www.deviantart.com/_napi/shared_api/comments/thread", params={'itemid': threadId, 'cursor': cursor}).json()
             for comment in threadSegment['thread']:
                 username = comment['user']['username']
                 if username in searchRequests:
                     #write to file we got a link
-                    print(username+" posted: ")
-                    print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+                    # print(username+" posted: ")
+                    # print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+                    count = count + 1
+                    print (count)
                 else:
                     for searchTerm in searchRequests:
                         if searchTerm in comment['textContent']['html']['markup']:
                             #write to file we got a link
-                            print(searchTerm+" mentioned in: ")
-                            print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+                            # print(searchTerm+" mentioned in: ")
+                            # print('https://www.deviantart.com/comments/1/'+ str(threadId) + '/' + str(comment['commentId']))
+                            count = count + 1
+                            print (count)
             cursor = threadSegment['cursor']
             if not threadSegment['hasMore']:
                 break
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from hardCodedInfo import threads, users
 from ScraperService import scrapeThread
 
-scrapeThread(threads, users)
+scrapeThread(threads, {'http://fav.me/dagmup2':['me'], '632601830': ['me'], 'https://tokotna.com/imports/index.php?id=20153': ['me']})
